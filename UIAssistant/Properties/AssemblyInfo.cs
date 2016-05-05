40ede713570e5a7b7fe0d50d23db5ffa9991a7dd
u338.steven <u338.steven@gmail.com>
2016-05-05 20:05:52 +0900

    Release 0.2

--- UIAssistant/Properties/AssemblyInfo.cs
+++ UIAssistant/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.2.2.0")]
+[assembly: AssemblyFileVersion("0.2.2.0")]
 [assembly: Guid("4D736133-7418-472C-A68A-484D66221BBC")]
 
