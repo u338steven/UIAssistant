971eb8b06d113b0fcd583e62baa864a1d91bec4b
u338.steven <u338.steven@gmail.com>
2016-12-01 17:53:58 +0900

    Add TimeMeasurement class

--- UIAssistant.Utility/Utility.cs
+++ UIAssistant.Utility/Utility.cs
@@ -11,4 +11,20 @@ namespace UIAssistant.Utility
     public static class Utility
     {
     }
+
+    public sealed class TimeMeasurement : IDisposable
+    {
+        private System.Diagnostics.Stopwatch _stopwatch;
+        public TimeMeasurement()
+        {
+            System.Diagnostics.Debug.WriteLine($"Start {DateTime.Now.ToShortTimeString()}");
+            _stopwatch = System.Diagnostics.Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+            System.Diagnostics.Debug.WriteLine($"Stop  {DateTime.Now.ToShortTimeString()} ({_stopwatch.ElapsedMilliseconds} ms)");
+        }
+    }
 }
