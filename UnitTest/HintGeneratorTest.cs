f502a261e7719ded00188cda7244d2c9943503e6
u338.steven <u338.steven@gmail.com>
2017-02-09 16:03:24 +0900

    Add UnitTest
    - HintGeneratorTest.cs

---
+++ UnitTest/HintGeneratorTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UIAssistant.Core.HitaHint;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class HintGeneratorTest
+    {
+        delegate IEnumerable<string> GenerateDelegate(string hintKeys, int quantity);
+
+        [TestMethod]
+        public void HintGeneratorTest1()
+        {
+            Enumerable.Range(1, 200).ToList().ForEach(x =>
+            {
+                GeneratorTest(HintGenerator.Generate, x, "asdfhjkl");
+            });
+        }
+
+        [TestMethod]
+        public void AlternateHintGeneratorTest1()
+        {
+            Enumerable.Range(1, 200).ToList().ForEach(x =>
+            {
+                GeneratorTest(AlternateHintGenerator.Generate, x, "asdf|hjkl");
+            });
+        }
+
+        static void GeneratorTest(GenerateDelegate func, int amount, string hintKeys)
+        {
+            var result = HintGenerator.Generate("asdfghjkl", amount);
+            Assert.AreEqual(amount, result.Count());
+            Assert.IsFalse(result.Any(x => result.Any(y => y.StartsWith(x) && y != x)));
+        }
+    }
+}
